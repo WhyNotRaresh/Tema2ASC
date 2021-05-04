14c1ff9a88c161a7fceb4a0991ec7c25f2737989
Rareș Octavian BĂDIȚĂ (100850) <rares.badita@stud.acs.upb.ro>
2021-05-04 19:59:46 +0300

    neoptim

--- solver_neopt.c
+++ solver_neopt.c
@@ -8,6 +8,36 @@
  * Add your unoptimized implementation here
  */
 double* my_solver(int N, double *A, double* B) {
-	printf("NEOPT SOLVER\n");
-	return NULL;
+	double *C = (double *) calloc(N * N, sizeof(double));
+	int i, j, k;
+
+	/* S1 = B * Bt */
+	double *S1 = (double *) calloc(N * N, sizeof(double));
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++){
+			for (k = 0; k < N; k++){
+				S1[i * N + j] += B[j * N + k] * B[j * N + k];
+			}
+		}
+	}
+
+	/* C = A * S1 */
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++){
+			for (k = j; k < N; k++){
+				C[i * N + j] += A[j * N + k] * S1[k * N + j];
+			}
+		}
+	}
+
+	/* C += A * At */
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++){
+			for (k = j; k < N; k++){
+				C[i * N + j] += A[j * N + k] * A[j * N + k];
+			}
+		}
+	}
+
+	return C;
 }
