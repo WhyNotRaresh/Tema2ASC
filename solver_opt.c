9f1b27e5937f19482a86003844315a51a69b2bb1
Rareș Octavian BĂDIȚĂ (100850) <rares.badita@stud.acs.upb.ro>
2021-05-05 18:17:57 +0300

    opt 3

--- solver_opt.c
+++ solver_opt.c
@@ -31,17 +31,10 @@ double* my_solver(int N, double *A, double* B) {
 
 	/* C = A * S1 */
 	for (i = 0; i < N; i++) {
-		double *line_ptr_A = A + i * N;
 		for (j = 0; j < N; j++){
-			double *line_ptr_A_copy = line_ptr_A + i;
-			double *col_ptr_S1 = S1 + j;
-			register double sum = 0;
 			for (k = i; k < N; k++){
-				sum += *line_ptr_A_copy * *col_ptr_S1;
-				line_ptr_A_copy++;
-				col_ptr_S1 += N;
+				C[i * N + j] += A[i * N + k] * S1[k * N + j];
 			}
-			C[i * N + j] = sum;
 		}
 	}
 	free(S1);
