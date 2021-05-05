d9ae42f3c53ef0eb04510b7979ff0f0e23f9d959
WhyNotRaresh <44733618+WhyNotRaresh@users.noreply.github.com>
2021-05-05 21:43:41 +0300

    New BLAS

--- solver_blas.c
+++ solver_blas.c
@@ -8,19 +8,19 @@
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A, double *B) {
-	double *AB = calloc(N * N, sizeof(double));
 	double *C = calloc(N * N, sizeof(double));
 
-	cblas_dcopy(N * N, B, 1, AB, 1);
+	/* C = B * Bt */
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0, B, N,
+		B, N, 1.0, C, N);
+	
+	/* C = A * C */
 	cblas_dtrmm(CblasRowMajor,CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit,
-		N, N, 1.0, A, N, AB, N);
+		N, N, 1.0, A, N, C, N);
 
+	/* C += At * A */
 	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1.0, A, N,
 		A, N, 1.0, C, N);
-
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0, AB, N,
-		B, N, 1.0, C, N);
-
-	free(AB);
+	
 	return C;
 }
