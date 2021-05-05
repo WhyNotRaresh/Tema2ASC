2fead22723f66489193d5f1309f7027fac469218
Rareș Octavian BĂDIȚĂ (100850) <rares.badita@stud.acs.upb.ro>
2021-05-05 18:52:47 +0300

    modified:   solver_opt.c

--- solver_opt.c
+++ solver_opt.c
@@ -48,8 +48,8 @@ double* my_solver(int N, double *A, double* B) {
 
 	/* C += A * At */
 	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++){
-			for (k = 0; k <= MIN(i, j); k++){
+		for (k = 0; k <= i; k++) {
+			for (j = 0; j < N; j++){
 				C[i * N + j] += A[k * N + i] * A[k * N + j];
 			}
 		}
