4b0809e552f04544bd35bad41f14737b705441ed
Rareș Octavian BĂDIȚĂ (100850) <rares.badita@stud.acs.upb.ro>
2021-05-05 18:13:28 +0300

    opt 2

--- solver_opt.c
+++ solver_opt.c
@@ -9,25 +9,39 @@
  * Add your unoptimized implementation here
  */
 double* my_solver(int N, double *A, double* B) {
-	double *C = (double *) calloc(N * N, sizeof(double));
+	double *C = (double *) malloc(N * N * sizeof(double));
 	int i, j, k;
 
 	/* S1 = B * Bt */
 	double *S1 = (double *) calloc(N * N, sizeof(double));
 	for (i = 0; i < N; i++) {
+		double *line_ptr_B = B + i * N;
 		for (j = 0; j < N; j++){
+			double *line_ptr_B_copy = line_ptr_B;
+			double *line_ptr_Bt = B + j * N;
+			register double sum = 0;
 			for (k = 0; k < N; k++){
-				S1[i * N + j] += B[i * N + k] * B[j * N + k];
+				sum += *line_ptr_B_copy * *line_ptr_Bt;
+				line_ptr_B_copy++;
+				line_ptr_Bt++;
 			}
+			S1[i * N + j] = sum;
 		}
 	}
 
 	/* C = A * S1 */
 	for (i = 0; i < N; i++) {
+		double *line_ptr_A = A + i * N;
 		for (j = 0; j < N; j++){
+			double *line_ptr_A_copy = line_ptr_A + i;
+			double *col_ptr_S1 = S1 + j;
+			register double sum = 0;
 			for (k = i; k < N; k++){
-				C[i * N + j] += A[i * N + k] * S1[k * N + j];
+				sum += *line_ptr_A_copy * *col_ptr_S1;
+				line_ptr_A_copy++;
+				col_ptr_S1 += N;
 			}
+			C[i * N + j] = sum;
 		}
 	}
 	free(S1);
